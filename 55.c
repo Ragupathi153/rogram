6b9cb0d72acf87b9a47c64d8617bd8143e80d8cf
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-19 21:28:44 +0530

    55.c

---
+++ 55.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,num;
+printf("enter the value");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+if(i%2==0)
+}
+printf("sum of all odd number");
+else
+printf("sum of all even number");
+}
