c3ceaaab899a02a444be57938c7a26911af5f494
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-21 16:50:17 +0530

    98.c

---
+++ 98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int x,arr[10],y;
+	scanf("%d",&x);
+	for(i=1;i<=y;i++)
+	{
+		scanf("%d",&arr[x]);
+		if(arr[x]!=x)
+		{
+			printf("%d",x);
+			break;
+		}
+	}
+	return 0;
+}
