b53832976969eba6f1877909e7f67add998645d0
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-21 16:57:15 +0530

    96.c

---
+++ 96.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int x,j,count=0;
+scanf("%d",&x);
+for(i=1;i<=n;i++)
+{
+if((x%j)==0)
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("no,not a composite");
+}
+else
+{
+printf("yes, composite");
+}
+getch();
+}
