cd02b9b81604ce1e7c67238fea4777003480334b
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-21 14:26:56 +0530

    100.c

---
+++ 100.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,mul=0,num,n[10];  
+scanf("%d",&num);
+printf("enter numbers");
+for(i=1;i<=num;i++)
+{
+scanf("%d",a[i]);
+mul=mul*a[i];
+}
+  printf("product=%d",mul);
+}
