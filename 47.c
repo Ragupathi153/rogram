dc04c93ca017da6f533d8599abf43da36f771bc9
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-21 14:07:48 +0530

    47.c

---
+++ 47.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+int arr max_n;
+    int i, max, min, n;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    printf("Enter elements in the array: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+ }
+    
+    for(i=1; i<n; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+ 
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d", min);
+
+    return 0;
+}
