2691d456f0b6dd26b8548b349e26443a587dff47
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-01-21 13:56:50 +0530

    48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main() 
+{
+int x,y,sum=0,num;
+ 
+  printf(" Enter How many Number you want");
+  scanf("%d",&y);
+ 
+  printf(" Enter the elements one by one");
+  for(i=0;i<y;++i)
+   {
+     scanf("%d",&num);
+     Sum = Sum +num;
+ 
+  return 0;
+}
